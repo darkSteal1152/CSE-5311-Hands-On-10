7e935d371b081104941d74a9caab79fa781602d1
darkSteal1152 <89552151+darkSteal1152@users.noreply.github.com>
2024-10-26 08:51:53 -0500

    Create binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,75 @@
+class BSTNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return BSTNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        else:
+            node.right = self._insert(node.right, key)
+        return node
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            min_larger_node = self._find_min(node.right)
+            node.key = min_larger_node.key
+            node.right = self._delete(node.right, min_larger_node.key)
+        return node
+
+    def _find_min(self, node):
+        while node.left is not None:
+            node = node.left
+        return node
+
+    def inorder(self):
+        result = []
+        self._inorder(self.root, result)
+        return result
+
+    def _inorder(self, node, result):
+        if node:
+            self._inorder(node.left, result)
+            result.append(node.key)
+            self._inorder(node.right, result)
+
+bst = BinarySearchTree()
+for value in [21, 17, 16, 5, 4, 1, 10]:
+    bst.insert(value)
+
+print(bst.inorder())
+bst.delete(17)
+print(bst.inorder())
